4d247d7692663900d370350f028df02cb2c769b9
Artur Cherkanov <artur.cherkanov@gmail.com>
2019-02-18 19:51:43 +0300

    Added Token Route to General File

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const eventRoutes = require('./events');
 const userRoutes = require('./users');
+const tokenRoutes = require('./token');
 
 router.use('/events', eventRoutes);
 router.use('/users', userRoutes);
+router.use('/token', tokenRoutes);
 
 module.exports = router;
