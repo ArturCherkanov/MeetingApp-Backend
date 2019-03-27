503db8107046111c9cbfc62c192daef3b3a81a42
Artur Cherkanov <artur.cherkanov@gmail.com>
2019-03-27 14:00:09 +0300

    Added New "Rooms" Route

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,12 @@ const router = express.Router();
 const eventRoutes = require('./events');
 const userRoutes = require('./users');
 const tokenRoutes = require('./token');
+const roomsRoutes = require('./rooms');
+
 
 router.use('/events', eventRoutes);
 router.use('/users', userRoutes);
+router.use('/rooms', roomsRoutes);
 router.use('/token', tokenRoutes);
 
 module.exports = router;
