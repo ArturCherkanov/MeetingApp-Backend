73ab092990b72a9fb0c9a38caf1679337472de34
Artur Cherkanov <artur.cherkanov@gmail.com>
2019-03-27 13:51:03 +0300

    Removed Waste Space

--- routes/token.js
+++ routes/token.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const middleware = require('../token-middleware');
 
-
 router.get('/', middleware.checkToken, (req, res) => {
     res.json({token: true})
     })
