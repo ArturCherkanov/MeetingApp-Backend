c57e02b8d2e4c9074aa5fc4ad20e02274f4bf7c6
Artur Cherkanov <artur.cherkanov@gmail.com>
2019-03-27 13:46:36 +0300

    Added New Model For Room

---
+++ models/Room.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// this will be our data base's data structure
+const RoomSchema = new Schema(
+  {
+    name: String,
+    time: Array,
+  },
+  { timestamps: true }
+);
+
+// export the new Schema so we could modify it using Node.js
+module.exports = mongoose.model("rooms", RoomSchema);
