83c789d2d4965d468ed448ac2f7a9ed6da2cc5e3
Artur <artur.cherkanov@gmail.com>
2019-05-16 00:23:00 +0300

    Changed hash structure

--- routes/user.js
+++ routes/user.js
@@ -8,15 +8,15 @@ router.get('/', middleware.checkToken, (req, res) => {
     res.json({
         token: true,
         email: req.decoded.username,
-        photoPath:'https://pp.userapi.com/M4on1Uicd_sK-hAx2kgAPcMPpnzDH7PS8xV_lQ/qqKPA4ZOEXo.jpg?ava=1'
+        photoPath: 'https://pp.userapi.com/M4on1Uicd_sK-hAx2kgAPcMPpnzDH7PS8xV_lQ/qqKPA4ZOEXo.jpg?ava=1',
     });
 });
 
 
 router.get('/list/', (req, res, next) => {
     User.find()
-        .then(users => { return userList = users.map(user => { return { name: user.firstname, email: user.username } }); })
-        .then(userList => res.send(userList))
+        .then(users => userList = users.map(user => { return { name: user.firstname, email: user.username } }))
+        .then(userList => res.send(userList));
 });
 
 module.exports = router;
